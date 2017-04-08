9c4150b32847453f162b14ead866154a53292c44
tonypie <tony.pierson@gmail.com>
2017-04-08 18:20:55 +0100

    Updated 8-4-17

--- Data/JokesDatabase.cs
+++ Data/JokesDatabase.cs
@@ -14,30 +14,89 @@ namespace Data
         private SqlConnection conn;
         public JokesDatabase()
         {
-            conn = GetConnection();
-
         }
         private SqlConnection GetConnection()
         {
             return new SqlConnection(ConfigurationManager.AppSettings["DatabaseConnection"].ToString());
         }
 
+        public Joke GetJoke(int jokeId)
+        {
+            using (conn = GetConnection())
+            {
+                List<Joke> jokes = new List<Joke>();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Jokes", conn);
+                SqlDataAdapter da = new SqlDataAdapter($"SELECT * FROM Jokes WHERE JokeID = { jokeId }", conn);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                Joke j = new Joke() { jokeId = (int)dt.Rows[0]["JokeID"], title = (string)dt.Rows[0]["Title"], jokeText = (string)dt.Rows[0]["Joke"] };
+
+                return j;
+            }
+        }
+
         public List<Joke> GetAllJokes()
         {
-            List<Joke> jokes = new List<Joke>();
-
-            SqlCommand cmd = new SqlCommand("SELECT * FROM Jokes", conn);
-            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Jokes", conn);
-            DataTable dt = new DataTable();
-            
-            da.Fill(dt);
-            foreach (DataRow row in dt.Rows)
+            using (conn = GetConnection())
             {
-                Joke j = new Data.Joke() { jokeId = (int)row["JokeID"], title = (string)row["Title"], jokeText = (string)row["Joke"] };
-                jokes.Add(j);
-            }
+                List<Joke> jokes = new List<Joke>();
 
-            return jokes;
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Jokes", conn);
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Jokes", conn);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+                foreach (DataRow row in dt.Rows)
+                {
+                    Joke j = new Data.Joke() { jokeId = (int)row["JokeID"], title = (string)row["Title"], jokeText = (string)row["Joke"] };
+                    jokes.Add(j);
+                }
+
+
+                return jokes;
+            }
+        }
+
+        public int AddJoke(Joke j)
+        {
+            int jokeId;
+
+            using (conn = GetConnection()) {
+                SqlCommand cmd = new SqlCommand() { Connection = conn, CommandType = CommandType.StoredProcedure, CommandText = "AddJoke" };
+                cmd.Parameters.AddWithValue("@Title", j.title);
+                cmd.Parameters.AddWithValue("@JokeText", j.jokeText);
+                conn.Open();
+                jokeId = Convert.ToInt32(cmd.ExecuteScalar());
+
+                return jokeId;
+            }
+        }
+
+        public void UpdateJoke(Joke j)
+        {
+            using (conn = GetConnection())
+            {
+                SqlCommand cmd = new SqlCommand() { Connection = conn, CommandType = CommandType.StoredProcedure, CommandText = "UpdateJoke" };
+                cmd.Parameters.AddWithValue("@JokeID", j.jokeId);
+                cmd.Parameters.AddWithValue("@Title", j.title);
+                cmd.Parameters.AddWithValue("@JokeText", j.jokeText);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteJoke(int jokeId)
+        {
+            using (conn = GetConnection())
+            {
+                SqlCommand cmd = new SqlCommand() { Connection = conn, CommandType = CommandType.StoredProcedure, CommandText = "DeleteJoke" };
+                cmd.Parameters.AddWithValue("@JokeID", jokeId);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
--- DataTestApplication/Program.cs
+++ DataTestApplication/Program.cs
@@ -12,7 +12,20 @@ namespace DataTestApplication
         static void Main(string[] args)
         {
             JokesDatabase jdb = new JokesDatabase();
+            
+
+            //Try the create method
+            //Joke j = new Joke() { title = "Postcard", jokeText = "My wife told me: ‘Sex is better on holiday.’ That wasn’t a very nice postcard to receive." };
+            //jdb.AddJoke(j);
+
             List<Joke> l = jdb.GetAllJokes();
+            foreach(Joke jo in l)
+            {
+                Console.WriteLine($"Joke ID : {jo.jokeId }, Title : { jo.title }, Joke : { jo.jokeText }");
+            }
+
+            Joke jok = jdb.GetJoke(4);
+            Console.WriteLine($"Joke ID : {jok.jokeId }, Title : { jok.title }, Joke : { jok.jokeText }");
         }
     }
 }
